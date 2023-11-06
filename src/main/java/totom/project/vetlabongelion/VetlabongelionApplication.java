84d0e05d042c85f8d7b34a0c286b23bb8ae538b4
T0Tom <lapinkirill.apple@mail.ru>
2023-11-06 12:17:24 +1200

    initial commit

---
+++ src/main/java/totom/project/vetlabongelion/VetlabongelionApplication.java
@@ -0,0 +1,13 @@
+package totom.project.vetlabongelion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetlabongelionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetlabongelionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/totom/project/vetlabongelion/VetlabongelionApplicationTests.java
@@ -0,0 +1,13 @@
+package totom.project.vetlabongelion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetlabongelionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
